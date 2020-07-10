179eed933b14a2db1277fd7719b27e10ef17a535
gabriell325 <gabrielmartines145@gmail.com>
2020-07-09 22:16:26 -0300

    prova

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
-start();
+Start();
+let table = document.querySelector('#table');
+let body = table.childNodes[3]
 
-function start() {}
+let list = document.querySelectorAll('input');
+
+list.forEach(input => {
+
+    input.addEventListener("blur", i => {
+      
+        let string = i.target.value;      
+        if(string.indexOf('+') != -1){
+            let posicao = string.split("+")
+            let i = posicao[0].split("=");
+            let a = document.getElementById(i[1]).value;
+            let b = document.getElementById(`${posicao[1]}`).value;
+            let resultado = parseInt(a) + parseInt(b);
+            i.target.value = resultado;
+            i.target.innerHtml = resultado
+         
+        }else if(string.indexOf('-') != -1){
+            let posicao = string.split("-")
+            let i = posicao[0].split("=");
+            let a = document.getElementById(i[1]).value;
+            let b = document.getElementById(`${posicao[1]}`).value;
+            let resultado = parseInt(a) - parseInt(b);
+            i.target.value = resultado;
+            i.target.innerHtml = resultado
+        }
+    })
+})
